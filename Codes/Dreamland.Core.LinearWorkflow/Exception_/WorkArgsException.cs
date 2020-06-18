94cff923e4e5bca3123cb560c787e636c7e88148
Firito <yuzhefirito@gmail.com>
2020-06-18 15:37:14 +0800

    [modify] 补充WorkArgsException的构造方法；

--- Codes/Dreamland.Core.LinearWorkflow/Exception_/WorkArgsException.cs
+++ Codes/Dreamland.Core.LinearWorkflow/Exception_/WorkArgsException.cs
@@ -1,8 +1,56 @@
 ﻿using System;
+using System.Runtime.Serialization;
+using Newtonsoft.Json;
 
 namespace Dreamland.Core.LinearWorkFlow
 {
+    [Serializable]
     public class WorkArgsException : ArgumentException
     {
+        private readonly WorkArgs _workArgs;
+
+        public WorkArgsException() : base()
+        {
+        }
+
+        public WorkArgsException(WorkArgs args) : base()
+        {
+            _workArgs = args;
+        }
+
+        public WorkArgsException(string message) : base(message)
+        {
+        }
+
+        public WorkArgsException(string message, Exception innerException)
+            : base(message, innerException)
+        {
+        }
+
+        public WorkArgsException(string message, string paramName, Exception innerException)
+            : base(message, paramName, innerException)
+        {
+        }
+
+        public WorkArgsException(string? message, string paramName)
+            : base(message, paramName)
+        {
+        }
+
+        protected WorkArgsException(SerializationInfo info, StreamingContext context)
+            : base(info, context)
+        {
+        }
+
+        public override string Message
+        {
+            get
+            {
+                var str = base.Message;
+                if (_workArgs?.PropertyNameList != null)
+                    str = str + $"[The current additional parameter is {JsonConvert.SerializeObject(_workArgs.PropertyNameList)}]";
+                return str;
+            }
+        }
     }
 }
