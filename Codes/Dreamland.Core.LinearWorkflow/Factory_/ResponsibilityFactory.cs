bc1ce0f33d6491abd6095af76ebfb23cff068908
Firito <yuzhefirito@gmail.com>
2020-06-24 10:01:37 +0800

    [modify] 在工厂执行中，使用参数的副本进行传递，防止执行中的参数暴露；增加可选择对执行过程中的全部生成结果进行输出；

--- Codes/Dreamland.Core.LinearWorkflow/Factory_/ResponsibilityFactory.cs
+++ Codes/Dreamland.Core.LinearWorkflow/Factory_/ResponsibilityFactory.cs
@@ -29,26 +29,45 @@ namespace Dreamland.Core.LinearWorkFlow
         ///     按顺序执行工作，如果某个<see cref="IResponsibilityWorker" />无法执行，则跳过该 <see cref="IResponsibilityWorker" />
         ///     前一个附加的执行信息会附加到下一个工作者的工作参数中
         /// </summary>
-        /// <param name="args"></param>
+        /// <param name="args">工作参数</param>
         /// <param name="token"></param>
+        /// <param name="isOutputAttachedResult">是否输出附加结果</param>
         /// <returns></returns>
-        public async Task<WorkResult> ExecuteWorkAsync(WorkArgs args, CancellationToken token)
+        public async Task<WorkResult> ExecuteWorkAsync(WorkArgs args, CancellationToken token,
+            bool isOutputAttachedResult = false)
         {
             var result = new WorkResult {IsSuccessful = true};
+            var resultContents = new Dictionary<string, object>();
+
+            //创建一个新的工作参数的副本用于工作流程中
+            var innerArgs = new WorkArgs();
+            foreach (var propertyName in args.PropertyNameList)
+                if (args.GetProperty(propertyName, out object obj))
+                    innerArgs.SetProperty(propertyName, obj);
 
             foreach (var worker in Workers)
             {
-                if (worker.CanExecuteWork(args))
-                {
-                    result = await worker.ExecuteWorkAsync(args, token).ConfigureAwait(false);
-                    foreach (var propertyName in result.PropertyNameList)
-                        if (result.GetProperty(propertyName, out object obj))
-                            args.SetProperty(propertyName, obj);
-                }
+                if (!worker.CanExecuteWork(innerArgs)) continue;
 
-                if (!result.IsSuccessful) return result;
+                result = await worker.ExecuteWorkAsync(innerArgs, token).ConfigureAwait(false);
+                foreach (var propertyName in result.PropertyNameList)
+                    if (result.GetProperty(propertyName, out object obj))
+                    {
+                        innerArgs.SetProperty(propertyName, obj);
+                        resultContents.SetAttachedProperty(propertyName, obj);
+                    }
+
+                if (!result.IsSuccessful)
+                {
+                    break;
+                }
             }
 
+            if (!isOutputAttachedResult) return result;
+
+            //如果输出时需要附加结果，则将执行过程中产生的结果附加到返回值中
+            foreach (var (key, value) in resultContents)
+                result.SetProperty(key, value);
             return result;
         }
     }
